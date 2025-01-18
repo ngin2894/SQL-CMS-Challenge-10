935ac03d347533c3b39d0bdc23fa2c85efd0d66f
ngin2894 <nataliegindraux@gmail.com>
2025-01-18 10:38:13 -0600

    added seeds file and finished the code to create tables in the schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,29 @@
 DROP DATABASE if EXISTS employee_tracker;
 CREATE DATABASE employee_tracker;
 
-\c employee_tracker;
+DROP TABLE IF EXISTS employee CASCADE;
+DROP TABLE IF EXISTS role CASCADE;
+DROP TABLE IF EXISTS department CASCADE;
 
 CREATE TABLE department (
     id SERIAL PRIMARY KEY,
     name VARCHAR(30) UNIQUE NOT NULL
+);
+
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department (id)
+);
+
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role (id),
+    FOREIGN KEY (manager_id) REFERENCES employee (id)
 );
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (name) VALUES ('Engineering'), ('HR');
+
+INSERT INTO role (title, salary, department_id) VALUES ('Engineer', 100000, 1), ('HR Manager', 80000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id) VALUES ('Alice', 'Johnson', 1), ('Bob', 'Smith', 2);
